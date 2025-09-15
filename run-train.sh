8d82462401e705f396632f674f7cde23a6ff357c
AlexNmSED <42379333+AlexNmSED@users.noreply.github.com>
2025-09-15 14:15:15 +0800

    Add files via upload

---
+++ run-train.sh
@@ -0,0 +1,15 @@
+python train_mil.py --yaml_path /n/archive00/labs/IT/GDC/xihao/HIPT-master-org/2-Weakly-Supervised-Subtyping/MIL_BASELINE-2/configs/CLAM_MB_MIL.yaml  
+
+python train_mil.py --yaml_path /n/archive00/labs/IT/GDC/xihao/HIPT-master-org/2-Weakly-Supervised-Subtyping/MIL_BASELINE-2/configs/CLAM_SB_MIL.yaml
+
+python train_mil.py --yaml_path /n/archive00/labs/IT/GDC/xihao/HIPT-master-org/2-Weakly-Supervised-Subtyping/MIL_BASELINE-2/configs/AMD_MIL.yaml   
+  
+python train_mil.py --yaml_path /n/archive00/labs/IT/GDC/xihao/HIPT-master-org/2-Weakly-Supervised-Subtyping/MIL_BASELINE-2/configs/DGR_MIL.yaml  
+
+python train_mil.py --yaml_path /n/archive00/labs/IT/GDC/xihao/HIPT-master-org/2-Weakly-Supervised-Subtyping/MIL_BASELINE-2/configs/FR_MIL.yaml   
+
+python train_mil.py --yaml_path /n/archive00/labs/IT/GDC/xihao/HIPT-master-org/2-Weakly-Supervised-Subtyping/MIL_BASELINE-2/configs/RRT_MIL.yaml   
+
+python train_mil.py --yaml_path /n/archive00/labs/IT/GDC/xihao/HIPT-master-org/2-Weakly-Supervised-Subtyping/MIL_BASELINE-2/configs/WIKG_MIL.yaml 
+
+python train_mil.py --yaml_path /n/archive00/labs/IT/GDC/xihao/HIPT-master-org/2-Weakly-Supervised-Subtyping/MIL_BASELINE-2/configs/AB_MIL.yaml    
